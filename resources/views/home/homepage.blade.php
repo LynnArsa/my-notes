8a2d62e82343fc889bae4f777142f5ee7be47fce
LynnArsa <lintang13arsa@gmail.com>
2023-06-13 01:51:39 +0700

    fix js

--- resources/views/home/homepage.blade.php
+++ resources/views/home/homepage.blade.php
@@ -51,69 +51,60 @@
 
 <script>
 document.addEventListener("DOMContentLoaded", function() {
-    const bgBodyElements = document.querySelectorAll(".bg-body");
-    const rightContent = document.getElementById("rightContent");
-    let selectedElement = null;
+  const bgBodyElements = document.querySelectorAll(".bg-body");
+  const rightContent = document.getElementById("rightContent");
+  let selectedElement = null;
 
-    function createNoteElements(note) {
-        const titleElement = createEditableElement("p", "font-bold text-2xl", note.title);
-        const dateElement = createDivElement("mb-4", note.date);
-        const bodyElement = createEditableElement("div", null, note.body);
+  function createNoteElements(note) {
+    const titleElement = document.createElement("p");
+    titleElement.classList.add("font-bold", "text-2xl");
+    titleElement.textContent = note.title;
+    titleElement.contentEditable = "true";
 
-        return [titleElement, dateElement, bodyElement];
+    const dateElement = document.createElement("div");
+    dateElement.classList.add("mb-4");
+    dateElement.textContent = note.date;
+
+    const bodyElement = document.createElement("div");
+    bodyElement.textContent = note.body;
+    bodyElement.contentEditable = "true";
+
+    return [titleElement, dateElement, bodyElement];
+  }
+
+  function clearRightContent() {
+    rightContent.innerHTML = "";
+  }
+
+  function selectNoteElement(element) {
+    if (selectedElement !== null) {
+      selectedElement.classList.remove("bg-secondary");
     }
+    element.classList.add("bg-secondary");
+    selectedElement = element;
+  }
 
-    function createEditableElement(tag, className, content) {
-        const element = document.createElement(tag);
-        if (className) {
-            element.classList.add(className);
-        }
-        element.contentEditable = "true";
-        element.textContent = content;
-        return element;
-    }
+  function fetchNoteContent(noteId) {
+    fetch(`/notes/${noteId}`)
+      .then(response => response.json())
+      .then(note => {
+        clearRightContent();
+        const noteElements = createNoteElements(note);
+        noteElements.forEach(element => rightContent.appendChild(element));
+      })
+      .catch(error => console.error("Error:", error));
+  }
 
-    function createDivElement(className, content) {
-        const element = document.createElement("div");
-        element.classList.add(className);
-        element.textContent = content;
-        return element;
-    }
+  function handleNoteClick(element) {
+    selectNoteElement(element);
+    const noteId = element.dataset.noteId;
+    fetchNoteContent(noteId);
+  }
 
-    function clearRightContent() {
-        rightContent.innerHTML = "";
-    }
-
-    function selectNoteElement(element) {
-        if (selectedElement !== null) {
-            selectedElement.classList.remove("bg-secondary");
-        }
-        element.classList.add("bg-secondary");
-        selectedElement = element;
-    }
-
-    function fetchNoteContent(noteId) {
-        fetch(`/notes/${noteId}`)
-            .then(response => response.json())
-            .then(note => {
-                clearRightContent();
-                const noteElements = createNoteElements(note);
-                noteElements.forEach(element => rightContent.appendChild(element));
-            })
-            .catch(error => console.error("Error:", error));
-    }
-
-    function handleNoteClick(element) {
-        selectNoteElement(element);
-        const noteId = element.dataset.noteId;
-        fetchNoteContent(noteId);
-    }
-
-    bgBodyElements.forEach(element => {
-        element.addEventListener("click", () => handleNoteClick(element));
-    });
-});
-
+  bgBodyElements.forEach(element => {
+    element.addEventListener("click", () => handleNoteClick(element));
+  });
+  });
 </script>
 </body>
 </html>
