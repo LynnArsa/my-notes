0b3b44b990074fadd704c71f11a1f4787fe3ffdf
LynnArsa <lintang13arsa@gmail.com>
2023-06-13 15:43:59 +0700

    fix auth

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/notes/{note}', [HomepageController::class, 'show'])->name('notes.show');
     Route::put('/notes/{note}', [HomepageController::class, 'update'])->name('notes.update');
     Route::delete('/notes/{note}', [HomepageController::class, 'delete'])->name('notes.delete');
+    Route::get('/add', [AddController::class, 'create'])->name('notes.create');
+    Route::post('/add', [AddController::class, 'store']);
     Route::post('/logout', [HomepageController::class, 'logout'])->name('logout');
 });
 
+
 // Public Routes
 Route::middleware('guest')->group(function () {
-    Route::get('/add', [AddController::class, 'create'])->name('notes.create');
-    Route::post('/add', [AddController::class, 'store']);
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
     Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
