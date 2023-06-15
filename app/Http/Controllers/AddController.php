db0399ef2400c649caf05e6ed80b2e5d36e59e21
LynnArsa <lintang13arsa@gmail.com>
2023-06-16 00:00:52 +0700

    fe

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -16,18 +16,24 @@ class AddController extends Controller
     public function store(Request $request)
     {
         $user = Auth::user();
-    
+
+        // Validate the input
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
         // Create a new note with the provided title and body
         $note = new Note();
         $note->title = $request->input('title');
         $note->body = $request->input('body');
-        
+
         // Associate the note with the user by setting the user_id
         $note->user_id = $user->id;
-        
+
         // Save the note
         $note->save();
-    
+
         return redirect()->route('home.homepage')->with('success', 'Note created successfully.');
     }
 }
--- resources/views/notes/add.blade.php
+++ resources/views/notes/add.blade.php
@@ -3,54 +3,62 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Notes</title>
+    <title>Write Notes</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
+
 <body>
-    Hello please create note here
+    <nav>
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex items-center justify-between h-16">
+            <div class="justify-items-end">
+              <div class="flex-shrink-0">
+                <span class="font-bold text-[24px]">My <span class="text-secondary">Notes.</span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+    </nav>
+
 <form method="POST">
     @csrf
 
-    @if ($errors->any())
+@if ($errors->any())
+    <div>
         <div>
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $item)
-                        <li>{{ $item }}</li>
-                    @endforeach
-                </ul>
+            <ul>
+                @foreach ($errors->all() as $item)
+                    <li>{{ $item }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endif
+
+    <div class="flex flex-row">
+        <div class="container w-1/2  flex flex-col">
+            <div class="mx-auto">
+                <input class="p-2 px-96" type="text" name="title" placeholder="">
+            </div>
+            <div class="mx-auto">
+                <input class="p-48 px-96" type="text" name="body" placeholder="">
+            </div>
+            <div class="mx-auto p-12">
+                <button type="submit" class="px-[55px] py-[16px] bg-secondary rounded-lg flex">
+                    <img class="max-w-[22px]" src="https://raw.githubusercontent.com/LynnArsa/my-notes/main/public/Add%20White.png">
+                    <p class="text-white font-bold">Add</p>
+                </button>
             </div>
         </div>
-    @endif
-
-    <div>
-    <label>Title</label>
-        <input type="text" name="title" placeholder="Title">
-    </div>
-    <div>
-        <label>Body</label>
-        <input type="text" name="body" placeholder="Body">
-    </div>
-    <div>
-        <!-- <a href="{{ url('homepage') }}">
-        <button class="px-[55px] py-[16px] bg-red rounded-lg">
-          <img class="max-w-[22px] rotate-45" src="https://raw.githubusercontent.com/LynnArsa/my-notes/main/public/Add%20White.png">
-          <p class="text-white font-bold">Cancel</p>
-        </button>
-        </a> -->
-        <button type="submit" class="px-[55px] py-[16px] bg-secondary rounded-lg">
-          <img class="max-w-[22px]" src="https://raw.githubusercontent.com/LynnArsa/my-notes/main/public/Add%20White.png">
-          <p class="text-white font-bold">Add</p>
-        </button>
-    </div>
-    @yield('content')
+@yield('content')
 </form>
 
-<div class="flex items-center justify-center">
-    <div id="saveButtonContainer">
+        <div class="container w-1/2">
+            <div class="m-16">
+                <img class="max-w-[550px] mx-auto" src="https://raw.githubusercontent.com/LynnArsa/my-notes/main/public/Write.png">
+            </div>
+        </div>
     </div>
-  </div>
-
 </body>
 </html>
